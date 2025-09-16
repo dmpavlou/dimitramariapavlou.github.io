922ea0cdda7cbe8471588a93907ab0f030eddef1
Dimitra Pavlou <demmyp98@gmail.com>
2025-09-16 17:02:31 +0200

    content.config file

---
+++ src/content.config.ts
@@ -0,0 +1,21 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const projects = defineCollection({
+
+    loader: glob({ pattern: "**/*.md", base: "./src/content/projects" }),
+    schema: z.object({
+        title: z.string(),
+        published: z.date(),
+        description: z.string(),
+        tech: z.array(z.string()),
+        url: z.string()
+    })
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { projects };
