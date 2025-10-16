3648242435308ae2192a25d4754711061ff6a9a0
Dimitra Pavlou <demmyp98@gmail.com>
2025-10-16 17:47:45 +0200

    domain name set

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	
-	site: 'https://dimitramariapavlou.github.io',
+	site: 'https://dmpavlou.github.io',
 
 	vite: {
 		plugins: [tailwindcss()],
